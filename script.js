61ec9d79ec8af8752432bfde76062c9f7ffb78af
aobarbieri <aobarbieri@gmail.com>
2021-12-22 15:39:12 -0500

    Index.html: Script tag moved to the head and defer attribute added to it

--- script.js
+++ script.js
@@ -8,50 +8,50 @@ const loader = document.getElementById('loader');
 let apiQuotes = [];
 
 function showLoadingSpinner() {
-    loader.hidden = false;
-    quoteContainer.hidden = true;
+	loader.hidden = false;
+	quoteContainer.hidden = true;
 }
 
 function removeLoadingSpinner() {
-    quoteContainer.hidden = false;
-    loader.hidden = true;
+	quoteContainer.hidden = false;
+	loader.hidden = true;
 }
 
 function showNewQuote() {
-    showLoadingSpinner();
-    // Pick a random quote from quotes array
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    if (!quote.author) {
-        authorText.textContent = 'Unknown';
-    } else {
-        authorText.textContent = quote.author;
-    }
-    // Check Quote length to determine styling
-    if (quote.text.length > 70) {
-        quoteText.classList.add('long-quote');
-    } else {
-        quoteText.classList.remove('long-quote');
-    }
-    quoteText.textContent = quote.text;
-    removeLoadingSpinner();
+	showLoadingSpinner();
+	// Pick a random quote from quotes array
+	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+	if (!quote.author) {
+		authorText.textContent = 'Unknown';
+	} else {
+		authorText.textContent = quote.author;
+	}
+	// Check Quote length to determine styling
+	if (quote.text.length > 70) {
+		quoteText.classList.add('long-quote');
+	} else {
+		quoteText.classList.remove('long-quote');
+	}
+	quoteText.textContent = quote.text;
+	removeLoadingSpinner();
 }
 
 // Get Quotes from API
 async function getQuotes() {
-    showLoadingSpinner();
-    const apiUrl = 'https://type.fit/api/quotes';
-    try {
-        const response = await fetch(apiUrl);
-        apiQuotes = await response.json();
-        showNewQuote();
-    } catch (error) {
-        console.log(error);
-    }
+	showLoadingSpinner();
+	const apiUrl = 'https://type.fit/api/quotes';
+	try {
+		const response = await fetch(apiUrl);
+		apiQuotes = await response.json();
+		showNewQuote();
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 function tweetQuote() {
-    const twitterUrl = `https://twitter.com/intent/tweet?text="${quoteText.textContent}" - ${authorText.textContent}`;
-    window.open(twitterUrl, '_blank');
+	const twitterUrl = `https://twitter.com/intent/tweet?text="${quoteText.textContent}" - ${authorText.textContent}`;
+	window.open(twitterUrl, '_blank');
 }
 
 // Event Listeners
